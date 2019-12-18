cc582448c5c89a29fe2ba06acf3ed763acb559d7
Michal Púchy <puchy.michal@gmail.com>
2019-12-18 19:37:55 +0100

    Move states form converter to app and css input readonly implemented

--- src/App.js
+++ src/App.js
@@ -8,6 +8,23 @@ import Footer from './components/Footer'
 function App() {
   const [ currenciesList, setCurrenciesList ] = useState({})
   const [ statistics, setStatistics ] = useState({})
+  const [ conversion, setConversion ] = useState({
+    currencyFrom: 'HUF',
+    amountFrom: 0,
+    currencyTo: 'EUR',
+    amountTo: 0
+  })
+
+  const getStatistics = () => {
+    dataServices
+      .getStatistics()
+      .then(response => {
+        console.log("effect getStatistics service response: ", response)
+        setStatistics(response)
+      }).catch(error => {
+        console.log("effect getStatistics service error: ", error)
+      })
+  }
 
   useEffect(() => {
     dataServices
@@ -21,16 +38,40 @@ function App() {
   }, [])
 
   useEffect(() => {
-    dataServices
-      .getStatistics()
-      .then(response => {
-        console.log("effect getStatistics service response: ", response)
-        setStatistics(response)
-      }).catch(error => {
-        console.log("effect getStatistics service error: ", error)
-      })
+    getStatistics()
   }, [])
 
+  const handleFromChange = (event) => {
+    setConversion({...conversion, currencyFrom: event.target.value})
+  }
+  const handleToChange = (event) => {
+    setConversion({...conversion, currencyTo: event.target.value})
+  }
+  const handleAmount = (event) => {
+    setConversion({...conversion, amountFrom: event.target.value, amountTo: ''})
+  }
+  const handleSubmit = (event) => {
+    let newTransfer = {
+      fromCurrency: conversion.currencyFrom,
+      toCurrency: conversion.currencyTo,
+      fromAmount: conversion.amountFrom
+    }
+    dataServices
+      .convert(newTransfer)
+      .then(response => {
+        console.log("handleSubmit convert response: ", response)
+        setConversion({
+          currencyFrom: response.currencyFrom,
+          amountFrom: response.amountFrom,
+          currencyTo: response.currencyTo,
+          amountTo: response.amountTo
+        })
+      }).catch(error => {
+        console.log("handleSubmit convert error: ", error)
+      })
+    getStatistics()
+    event.preventDefault()
+  }
 
   return (
     <div className="App">
@@ -40,7 +81,7 @@ function App() {
       <main>
         <section>
           <h1>Converter</h1>
-          <Converter currenciesList={currenciesList} dataService={dataServices}/>
+          <Converter currenciesList={currenciesList} conversion={conversion} handleSubmit={handleSubmit} handleAmount={handleAmount} handleFromChange={handleFromChange} handleToChange={handleToChange} />
         </section>
         <section className="statistics">
           <h1>Statistics</h1>
--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,51 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import CurrencySelect from './CurrencySelect'
 
-const Converter = ({ currenciesList, dataService }) => {
-  const [conversion, setConversion] = useState({
-    currencyFrom: 'EUR',
-    amountFrom: 0,
-    currencyTo: 'CZK',
-    amountTo: 0
-  })
-
-  const handleFromChange = (event) => {
-    setConversion({...conversion, currencyFrom: event.target.value})
-  }
-  const handleToChange = (event) => {
-    setConversion({...conversion, currencyTo: event.target.value})
-  }
-  const handleAmount = (event) => {
-    setConversion({...conversion, amountFrom: event.target.value, amountTo: ''})
-  }
-  const handleSubmit = (event) => {
-    let newTransfer = {
-      fromCurrency: conversion.currencyFrom,
-      toCurrency: conversion.currencyTo,
-      fromAmount: conversion.amountFrom
-    }
-    dataService
-      .convert(newTransfer)
-      .then(response => {
-        console.log("handleSubmit convert response: ", response)
-        setConversion({
-          currencyFrom: response.currencyFrom,
-          amountFrom: response.amountFrom,
-          currencyTo: response.currencyTo,
-          amountTo: response.amountTo
-        })
-      }).catch(error => {
-        console.log("handleSubmit convert error: ", error)
-      })
-      event.preventDefault()
-  }
-
+const Converter = ({ currenciesList, conversion, handleSubmit, handleAmount, handleFromChange, handleToChange }) => {
+  
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <div className="line">
           <label className="col75">
-            <input value={conversion.amountFrom} type="number" onChange={handleAmount}/>
+            <input value={conversion.amountFrom} type="number" step="0.01" onChange={handleAmount}/>
           </label>
           <label className="col25">
             <CurrencySelect selected={conversion.currencyFrom} handler={handleFromChange} currencies={currenciesList} />
@@ -53,7 +16,7 @@ const Converter = ({ currenciesList, dataService }) => {
         </div>
         <div className="line">
           <label className="col75">
-            <input value={conversion.amountTo} type="number" readOnly/>
+            <input className="read" value={conversion.amountTo} type="number" readOnly/>
           </label>
           <label className="col25">
             <CurrencySelect selected={conversion.currencyTo} handler={handleToChange} currencies={currenciesList} />
